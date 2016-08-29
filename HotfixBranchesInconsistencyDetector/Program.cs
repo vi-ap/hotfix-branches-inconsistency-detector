7b4231d2ce747a1e9a88942cfd4117c14b2d54b6
vi-ap <aparna.23@outlook.com>
2016-08-29 00:42:40 -0700

    Adding function to check if program is running inside a git repo. Finds the repo root.

--- HotfixBranchesInconsistencyDetector/Program.cs
+++ HotfixBranchesInconsistencyDetector/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,33 @@ namespace HotfixBranchesInconsistencyDetector
     {
         static void Main(string[] args)
         {
+            string repoRoot = getRepoRootDir();
+            Console.WriteLine(repoRoot);
+            Console.ReadLine();
+        }
+        
+        private static string getRepoRootDir()
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            return getRepoRootDir(currentDirectory);
+        }
+
+        private static string getRepoRootDir(string currentDirectory)
+        {
+            Console.WriteLine(currentDirectory);
+            string gitDirPath = Path.Combine(currentDirectory, ".git");
+            if (Directory.Exists(gitDirPath))
+            {
+                return currentDirectory;
+            }
+
+            DirectoryInfo parentDirInfo = Directory.GetParent(currentDirectory);
+            if(parentDirInfo == null)
+            {
+                throw new Exception("No git repository found.");
+            }
+
+            return getRepoRootDir(parentDirInfo.FullName);
         }
     }
 }
